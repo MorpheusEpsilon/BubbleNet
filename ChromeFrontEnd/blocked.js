7d57674243abf2932347176986814cb5bbd034bc
“MorpheusEpsilon” <joseantoniomurphy@icloud.com>
2025-08-17 03:10:47 -0600

    changed logo, added frontend

--- ChromeFrontEnd/blocked.js
+++ ChromeFrontEnd/blocked.js
@@ -1,17 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get("blockedData", (data) => {
-    if (data.blockedData) {
-      document.getElementById("blocked-url").textContent = data.blockedData.url;
-      document.getElementById("adult-analysis").textContent =
-        data.blockedData.adult_analysis;
-      document.getElementById("kid-analysis").textContent =
-        data.blockedData.kid_analysis;
-    } else {
-      document.getElementById("blocked-url").textContent = "Unknown site";
-      document.getElementById("adult-analysis").textContent =
-        "No details available.";
-      document.getElementById("kid-analysis").textContent =
-        "No details available.";
+    const kidMessageElem = document.getElementById("kid-message");
+
+    if (kidMessageElem) {
+      if (data.blockedData && data.blockedData.kid_analysis) {
+        kidMessageElem.textContent = data.blockedData.kid_analysis;
+      } else {
+        kidMessageElem.textContent = "No details available.";
+      }
     }
   });
 });
