92c8099bc17b39ad00c12b6f48ffb138f1cf31ed
Charls-Ferat <a01739190@tec.mx>
2025-08-17 11:58:46 -0600

    Backend fixes

--- Backend/sms_alert.py
+++ Backend/sms_alert.py
@@ -1,6 +1,7 @@
 from twilio.rest import Client
 from Backend.config import settings
 import urllib.parse
+import os
 
 def send_alert_sms(to_number: str, site_url: str, analysis: str):
     #account_sid = os.getenv("TWILIO_ACCOUNT_SID")
@@ -15,14 +16,16 @@ def send_alert_sms(to_number: str, site_url: str, analysis: str):
     encoded_site = urllib.parse.quote(site_url, safe="")
     parent_url_base = "http://127.0.0.1:8000/control"
 
-    #message_body = (
-        #f"Alert: Your child accessed a suspicious site\n"
-        #f"AI Analysis: {analysis}\n\n"
-        #f"Take action: {parent_url_base}?site={encoded_site}"
-    #)
+    from_number = os.getenv("TWILIO_PHONE_NUMBER") or "+0000000000"
 
-    #client.messages.create(
-       # body=message_body,
-       # from_=from_number,
-       # to=to_number
-    #)
+    message_body = (
+        f"Alert: Your child accessed a suspicious site\n"
+        f"AI Analysis: {analysis}\n\n"
+        f"Take action: {parent_url_base}?site={encoded_site}"
+    )
+
+    client.messages.create(
+       body = message_body,
+       from_ = from_number,
+       to = to_number
+    )
