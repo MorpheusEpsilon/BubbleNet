c7f26e087a6cc7c06caf7f41355f1c7a12917c29
Charls-Ferat <a01739190@tec.mx>
2025-08-17 01:45:17 -0600

    Odio mi vida

--- ChromeFrontEnd/blocked.js
+++ ChromeFrontEnd/blocked.js
@@ -1,15 +1,11 @@
-// Recuperar datos del almacenamiento y mostrarlos en los textareas
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get("blockedData", (result) => {
     if (result.blockedData) {
       const { kid_analysis, adult_analysis } = result.blockedData;
-      document.getElementById("kid-message").value = kid_analysis || "No data";
-      document.getElementById("adultBox").value = adult_analysis || "No data";
-    }
-  });
 
-  // Botón "Ver más"
-  document.getElementById("seeMore").addEventListener("click", () => {
-    window.open("https://www.connectsafely.org/", "_blank");
+      // Insert AI analysis into the boxes
+      document.getElementById("kid-message").innerText =
+        kid_analysis || "No data available";
+    }
   });
 });
